a7c9760b922d580aa80d77a0e42d0e70f54484ec
mustack-rabbi <mustackrabbi@gmail.com>
2022-09-06 22:52:35 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Class 05 : Assignment"),
+      ),
+      body: Center(child: Text("Class 5")),
+    );
+  }
+}
