9958f604bf60027fb7e61893039ef474c953ac38
mustack-rabbi <mustackrabbi@gmail.com>
2022-09-09 20:36:07 +0600

    style file add

--- lib/utlis/app_styles.dart
+++ lib/utlis/app_styles.dart
@@ -10,4 +10,16 @@ class Styles {
   static Color bgColor = const Color(0xFf3b3b3b);
   static TextStyle textStyle =
       TextStyle(color: textColor, fontSize: 30, fontWeight: FontWeight.bold);
+  static TextStyle headLineStyle1 =
+      TextStyle(color: textColor, fontSize: 30, fontWeight: FontWeight.bold);
+  static TextStyle headLineStyle2 =
+      TextStyle(color: textColor, fontSize: 30, fontWeight: FontWeight.bold);
+  static TextStyle headLineStyle3 =
+      TextStyle(color: textColor, fontSize: 30, fontWeight: FontWeight.bold);
+  static TextStyle headLineStyle4 =
+      TextStyle(color: textColor, fontSize: 30, fontWeight: FontWeight.bold);
+  static TextStyle headLineStyle5 =
+      TextStyle(color: textColor, fontSize: 30, fontWeight: FontWeight.bold);
+  static TextStyle headLineStyle6 =
+      TextStyle(color: textColor, fontSize: 30, fontWeight: FontWeight.bold);
 }
